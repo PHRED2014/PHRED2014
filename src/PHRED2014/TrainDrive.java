f9e5453eb36712bcb5a4ef032fa58e8a71bfa832
svaebog <svaebog@gmail.com>
2014-02-05 14:01:14 -0800

    Removed Servo code from Autonomous, and commented out the servo code in TrainDrive.  Servo code causes problems on robots that do not have servos installed.

--- src/PHRED2014/TrainDrive.java
+++ src/PHRED2014/TrainDrive.java
@@ -20,16 +20,20 @@ public class TrainDrive implements RobotMap{
     private double YJoy = 0;
     private double ZJoy = 0;
     private OI COVOP;
-    private Servo ServoI;
-    private Servo ServoII;
+    private Servo panServo;
+    private Servo tiltServo;
     
     //Contructor(s)
     public TrainDrive(OI oi){
          driveMotors = new RobotDrive(1,2,3,4);
          COVOP = oi;
-         ServoI = new Servo(1);
-         ServoII = new Servo(2);
     }
+         
+/* Uncomment when servos get installed         
+         panServo = new Servo(ServoI);
+         tiltServo = new Servo(ServoII);
+    }
+*/
     
     //Methods(functions)
     public void Safety(boolean onf){
@@ -48,18 +52,15 @@ public class TrainDrive implements RobotMap{
         driveMotors.setInvertedMotor(RobotDrive.MotorType.kFrontRight, true);
         driveMotors.setInvertedMotor(RobotDrive.MotorType.kRearRight, true);
     }    
-    
-    
-    
   
-  
-  public void setServo(){
-      double servoIangle = ServoI.getAngle();
-      double servoIIangle = ServoII.getAngle();
+/* Uncomment when servos get installed
+    public void setServo(){
+        double servoIangle = panServo.getAngle();
+        double servoIIangle = tiltServo.getAngle();
       
-      ServoI.setAngle(0.0);
-      ServoII.setAngle(0.0);
-  }
-  
+        panServo.setAngle(0.0);
+        tiltServo.setAngle(0.0);
+    }
+ */
 }
 
