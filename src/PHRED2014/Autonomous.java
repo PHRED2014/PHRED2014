a5572939d3f1920340f8fb071147cbacb143a58b
KVPhred2014 <kyle.vanorden@gmail.com>
2014-03-29 11:45:18 -0700

    Autonomous changes

--- src/PHRED2014/Autonomous.java
+++ src/PHRED2014/Autonomous.java
@@ -1,33 +1,43 @@
 
 package PHRED2014;
-
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class Autonomous implements RobotMap{
     
     //Instance variables
     private TrainDrive trainDrive;
+    private ObjM ObjMan;
     
     private PHREDSonic usFore = null;
     private PHREDSonic usAft = null;
     private PHREDSonic usForward = null;
 
-    String status;
-    private double rangeFore, rangeAft, rangeDiff, stopRange, rangeTolerance = 0.0;
-    private double turnSpeed, driveSpeed, rangeForward, plSpeed, prSpeed = 0.0;
-
+    private double rangeFore, rangeAft, rangeDiff, endOfFirstLeg, endOfSecondLeg,
+            rangeTolerance, turnSpeed, flDriveSpeed, slDriveSpeed, rangeForward,
+            timeOut = 0.0;
+    
+    private boolean firstLeg;
+    
     //Contstructor(s)
-    public Autonomous(TrainDrive td, int autoID, double[] autoSpeedSettings){
+    public Autonomous(TrainDrive td, ObjM om){
         trainDrive = td;
-        
-        stopRange = autoSpeedSettings[SCORE_RANGE_IDX - 1];
-        rangeTolerance = autoSpeedSettings[RANGE_TOLERANCE_IDX - 1];
-        turnSpeed = autoSpeedSettings[TURN_SPEED_IDX - 1];
-        driveSpeed = autoSpeedSettings[DRIVE_SPEED_IDX - 1];
-        
-        //if(usForward == null)usForward = new PHREDSonic(FRONT_ULTRA_P, FRONT_ULTRA_E);
-        usForward = new PHREDSonic(9,10);
+        ObjMan = om;
+    }//End Constructor
+    
+    //Methods
+    public void initialize(int autoID, double[] autoSpeedSettings){
+        flDriveSpeed = autoSpeedSettings[FL_DRIVE_SPEED_IDX - 1] * -1;//Initialize to first leg drive speed
+        slDriveSpeed = autoSpeedSettings[SL_DRIVE_SPEED_IDX - 1] * -1;//Initialize to second leg drive speed
 
+        endOfFirstLeg = autoSpeedSettings[FL_RANGE_TO_GOAL_IDX - 1]; //1800 - Distance in millimeters from the goal
+        endOfSecondLeg = autoSpeedSettings[SL_RANGE_TO_GOAL_IDX - 1]; //450
+
+        rangeTolerance = 12.7; //The allowable difference in range between the two side ultrasonic sensors
+        turnSpeed = 0.5; //The 
+//
+        firstLeg = true;
+        
+        if(usForward == null)usForward = new PHREDSonic(FRONT_ULTRA_P, FRONT_ULTRA_E);
         switch(autoID){
             case WALL_LEFT:{
                 if(usFore == null)usFore = new PHREDSonic(LEFT_FRONT_ULTRA_P,LEFT_FRONT_ULTRA_E);
@@ -41,89 +51,123 @@ public class Autonomous implements RobotMap{
             }
             case CENTER:
             default:
-                stopRange = 1000;// ~10 feet
+                Utils.timeReset();
+                Utils.timeStart();
+                flDriveSpeed = -0.5;//Initialize to first leg drive speed
+                timeOut = 3.0; //Seconds
+//                endOfFirstLeg = 3000;// ~10 feet
                 break;
         }//End switch
-        
-        Utils.timeReset();
-        Utils.timeStart();
-    }//End Constructor
-    
-    //Methods
-    public void driveForward(){
-        rangeForward = getTheRange(usForward);
-        SmartDashboard.putNumber("E Time: ", Utils.timeElapsed());        
-        SmartDashboard.putNumber("Forward: ", rangeForward);
 
-        if(rangeForward > stopRange){driveForGoal(STRAIGHT);}
-        else{driveForGoal(STOP);}
+    }//End autoInit
+    
+    public void driveForward(){
+        if(Utils.timeElapsed() < timeOut)
+            driveForGoal(STRAIGHT);
+        else
+            driveForGoal(STOP);
+        
+        pl("Elapsed Time ", Utils.timeElapsed());
     }
     
-    public void scrapeTheWall(int script){
-        rangeForward = getTheRange(usForward);
+    public void scoreAGoal(int script){
+        
+        if(firstLeg){ //The first of two legs: Tankdrive parallel to the wall until 5-6ft from goal
+            //Get the range forward
+            rangeForward = getTheRange(usForward);
+            pl("Range Forward: ", rangeForward);
 
-        if(rangeForward > stopRange){
-            rangeFore = getTheRange(usFore);
-            rangeAft = getTheRange(usAft);
-            rangeDiff = rangeFore - rangeAft;
+            if(rangeForward <= endOfFirstLeg){
+                driveForGoal(STOP);
+                firstLeg = false;
+            }else{
+                //Get the range to the wall at the front of the robot
+                rangeFore = getTheRange(usFore);
             
-            if(rangeTolerance <= Math.abs(rangeDiff)){
-                if(rangeDiff > 0){
-                    if(script == WALL_LEFT){driveForGoal(TURN_LEFT);}
-                    else{driveForGoal(TURN_RIGHT);}
-                }else if(script == WALL_LEFT){driveForGoal(TURN_RIGHT);}
-                      else{driveForGoal(TURN_LEFT);}
-            }else{driveForGoal(STRAIGHT);}
-        }else{
-            Utils.timeStop();
-            driveForGoal(STOP);
-            strafeToScorePosition();
-            scoreTheGoal();
+                //Get the range to the wall at the rear of the robot
+                rangeAft = getTheRange(usAft);
+            
+                //Calculate the difference between the front and rear ranges
+                rangeDiff = rangeFore - rangeAft;
+                System.out.println("Range F,A,D: "+rangeFore+" "+rangeAft+" "+rangeDiff);
+            
+                //Check for the need to correct course
+                if(rangeTolerance <= Math.abs(rangeDiff)){
+                    if(rangeDiff > 0){
+                    
+                        //Front of robot farthest from wall: course correction needed
+                        if(script == WALL_LEFT){driveForGoal(TURN_LEFT);}
+                        else{driveForGoal(TURN_RIGHT);}
+                    
+                    //Rear of robot farthest from wall: course correction needed
+                    }else if(script == WALL_LEFT){driveForGoal(TURN_RIGHT);}
+                          else{driveForGoal(TURN_LEFT);}
+                
+                //Within tolerance so no course correction
+                }else{driveForGoal(STRAIGHT);}
+            }
+        }else{ //The second Leg: Mechanum drive at a 45 deg angle towards the wall/goal
+            //Get the range forward
+            rangeForward = getTheRange(usForward);
+            pl("Range Forward: ", rangeForward);
+
+            //Check to see if the robot is in scoring position
+            if(rangeForward <= endOfSecondLeg){
+                driveForGoal(STOP);
+                scoreTheGoal();
+            }else{
+                if(script == WALL_LEFT)
+                    driveForGoal(MECANUM_LEFT);
+                else
+                    driveForGoal(MECANUM_RIGHT);
+            }
         }
-        sdPost();
     }
 
     private void driveForGoal(int direction){
-        double lSpeed, rSpeed;
-        lSpeed = rSpeed = -driveSpeed;
-        
         switch (direction){
-            case TURN_LEFT: lSpeed *= turnSpeed; status = "Turn Left"; break;
-            case TURN_RIGHT: rSpeed *= turnSpeed; status = "Turn Right"; break;
-            case STRAIGHT: status = "Drive Straight"; break;
+            case TURN_LEFT:    trainDrive.driveLikeATank(flDriveSpeed * turnSpeed, flDriveSpeed);
+                               pl("Turn Left"); break;
+            case TURN_RIGHT:   trainDrive.driveLikeATank(flDriveSpeed, flDriveSpeed * turnSpeed);
+                               pl("Turn Right"); break;
+            case STRAIGHT:     trainDrive.driveLikeATank(flDriveSpeed, flDriveSpeed);
+                               pl("Drive Straight"); break;
+   /*       case MECANUM_LEFT: trainDrive.MechaDrive(slDriveSpeed, slDriveSpeed, 0.0);
+                               pl("Mecanum Left"); break;
+            case MECANUM_RIGHT:trainDrive.MechaDrive(-slDriveSpeed, slDriveSpeed, 0.0);
+                               pl("Mecanum Right"); break;
+   */
             case STOP:
-            default: lSpeed = rSpeed = 0.0; status = "Stop"; break;
+            default:           trainDrive.driveLikeATank(0.0, 0.0);
+                               pl("Stop"); break;
         }//End Switch
-
-        if(plSpeed != lSpeed || prSpeed != rSpeed){
-            plSpeed = lSpeed; prSpeed = rSpeed;
-            trainDrive.driveLikeATank(lSpeed, rSpeed);
-        }
     }//End driveForGoal
 
-    private void strafeToScorePosition(){
-//TODO: Strafe left or right to center ball on goal        
-    }
-    
     private void scoreTheGoal(){
-//TODO: Fork to position, and eject the ball
+        //if(ObjMan.moveForks())//Returns true when forks in scoring position
+        //    ObjMan.TankBelt();
     }//End scoreTheGoal
     
     private double getTheRange(PHREDSonic us){
-        double range;
-        while(true){
-            us.ping();
+        double range = 0.0;
+        double maxRange = 4500; //~15 Feet
+        timeOut = 0.010;
+        
+        us.ping();
+        Utils.timeReset();
+        Utils.timeStart();
+        
+        while(Utils.timeElapsed() < timeOut){
             if((range = us.getRangeMM())!= 0.0)break;
         }
+        if(range == 0.0 || range > maxRange)
+            range = maxRange;
         return range;
     }
 
-    private void sdPost(){
-        SmartDashboard.putNumber("E Time: ", Utils.timeElapsed());        
-        SmartDashboard.putNumber("Front: ", rangeForward);
-        SmartDashboard.putNumber("LeftFront: ", rangeFore);
-        SmartDashboard.putNumber("LeftRear: ", rangeAft);
-        SmartDashboard.putNumber("F/R Diff: ", rangeDiff);
-        SmartDashboard.putString("Robot Status: ", status);
-    }
+    //I'm tired of typing System.out.println
+    public void pl(String s){System.out.println(s); SmartDashboard.putString(s, s);}
+    public void pl(String s1, String s2){System.out.println(s1 + s2); SmartDashboard.putString(s1, s2);}
+    public void pl(String s, int i){System.out.println(s + i); SmartDashboard.putNumber(s, i);}
+    public void pl(String s, double d){System.out.println(s + d); SmartDashboard.putNumber(s, d);}
 }
